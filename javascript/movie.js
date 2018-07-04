6955d37faee7fbf54fe4332e2bcc3720baf6675f
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-03 22:33:22 -0400

    fetching the data from api

--- javascript/movie.js
+++ javascript/movie.js
@@ -1,3 +1,40 @@
 $(document).ready(()=>{
-    alert('hey');
+    //create an event when th form is submitted
+    $('#searchform').on('submit', (e)=>{
+        let searchtext = $('#searchtext').val();
+        getMovies(searchtext);
+        // console.log($(searchtext).val());
+        e.preventDefault();
+    })
 });
+
+function getMovies(searchtext){
+    // console.log(searchtext);
+    axios.get('http://www.omdbapi.com?s='+searchtext+'&apikey=2baf180')
+    .then((response) => {
+        console.log(response);
+        let movies = response.data.Search;
+        console.log(movies);
+        let output = '';
+        $.each(movies, (index, movie) => {
+            output += `
+            <div class="col-md-3">
+            <div class="well text-center">
+            <img src = "${movie.Poster}">
+            <h5>${movie.Title}</h5>
+            <h6>${movie.Year}</h6>
+            <a onclick="movieselected('${movie.imdbID}')" class="btn btn-primary" href="#">More details</a>
+            </div>
+            </div>
+             `
+        })
+        $('#movies').html(output);
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
+}
+
+// var callback = function(user){console.log(user)};
+// callback(user);
+
